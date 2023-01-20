975fd49b78b60ad875e7a661020d0063b39ca359
Tuvshuu <tuvshuu@mirai.mn>
2023-01-20 20:59:51 +0800

    don't forget to delete your changes

--- TBCamp/seeds/index.js
+++ TBCamp/seeds/index.js
@@ -17,14 +17,19 @@ async function main() {
 const sample = (arr) => {
   const rand = Math.floor(Math.random() * arr.length);
   return arr[rand];
-}
+};
 
 const InitData = () => {
   for (let i = 0; i < 50; i++) {
+    const price = Math.floor(Math.random() * 50) * 10;
     const rand = Math.floor(Math.random() * 1000);
     const campground = new Campground({
       title: `${sample(descriptors)} ${sample(places)}`,
       location: `${locations[rand].city}, ${locations[rand].state}`,
+      imgUrl: "https://source.unsplash.com/random?InTheWoods",
+      price: price,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     });
 
     console.log(campground);
