e5e3b511a39f6da57de0b40b8224ce732ee19a43
Tuvshuu <tuvshuu@mirai.mn>
2023-02-02 20:55:16 +0800

    tb camp update

--- ShopApp/models/farm.js
+++ ShopApp/models/farm.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Product = require('./product');
 
 mongoose.set('strictQuery', false);
 
@@ -14,6 +15,18 @@ const farmSchema = new mongoose.Schema({
     products: [
         { type: mongoose.Schema.Types.ObjectId, ref: 'Product' }
     ]
+})
+
+farmSchema.pre('findOneAndDelete', async (data)=>{
+    console.log('Pre is called');
+    console.log(data);
+})
+
+farmSchema.post('findOneAndDelete', async (farm)=>{
+    console.log('Post is called');
+    console.log(farm);
+    if(farm.products.length)
+        await Product.deleteMany({_id: {$in: farm.products}});
 })
 
 module.exports = mongoose.model("Farm", farmSchema);
