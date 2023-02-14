83d97c8f5ada59cb32e8cbdd993f11e469e6df0e
Tuvshinbayar-hub <72744370+Tuvshinbayar-hub@users.noreply.github.com>
2023-02-15 00:47:14 +0800

    Logout and login mechanism have been implemented

--- authentication/index.js
+++ authentication/index.js
@@ -4,10 +4,9 @@ const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
 const path = require('path');
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 const User = require('./models/user');
-const { dir } = require('console');
-
 const app = express();
 
 app.engine(
@@ -26,6 +25,19 @@ app.set("view engine", "handlebars");
 app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.set("strictQuery", false);
 
+const sessionConfig = {
+    secret: 'Mojijojo123',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 3600 * 24 * 7,
+        maxAge: 1000 * 3600 * 24 * 7
+    }
+};
+app.use(session(sessionConfig));
+
+
 main().catch(e => console.log(e));
 
 async function main() {
@@ -41,7 +53,7 @@ async function main() {
 
     app.post('/register', async (req, res) => {
         const { username, password } = req.body;
-        const hashedPw = await bcrypt.hash(username, 12);
+        const hashedPw = await bcrypt.hash(password, 12);
 
         const user = new User({
             username,
@@ -50,6 +62,39 @@ async function main() {
 
         await user.save();
         res.redirect('/');
+    })
+    app.get('/login', (req, res) => {
+        res.render('./login');
+    })
+
+    app.post('/login', async (req, res) => {
+        const { username, password } = req.body;
+        const userToCheck = await User.findOne({ username: username });
+        if (userToCheck) {
+            const isValid = await bcrypt.compare(password, userToCheck.password)
+
+            console.log(isValid)
+            if (isValid) {
+                req.session.user_id = userToCheck._id;
+                res.render('./secret');
+            } else {
+                res.send('Name or pass wrong');
+            }
+        } else {
+            res.send('user not found');
+        }
+    });
+
+    app.get('/secret', (req, res) => {
+        if (req.session.user_id) {
+            return res.render('./secret');
+        }
+        res.render('./login');
+    });
+
+    app.post('/secret', (req, res) => {
+        req.session.user_id = null;
+        res.render('./login');
     })
 
     app.listen(3000, () => {
