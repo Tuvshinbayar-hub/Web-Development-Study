94902cbfc0d9d5ec7254940eefb59e7599f558eb
Tuvshuu <tuvshuu@mirai.mn>
2023-03-16 21:35:22 +0800

    Map first section 99% finished

--- TBCamp/public/js/showMap.js
+++ TBCamp/public/js/showMap.js
@@ -1,17 +1,8 @@
 
-// const key = process.env.MAPTILER_KEY;
-// console.log(key);
-// var map = new maplibregl.Map({
-//     container: 'map',
-//     style: `https://api.maptiler.com/maps/streets/style.json?key=${key}`, // stylesheet location
-//     center: [-74.5, 40], // starting position [lng, lat]
-//     zoom: 9 // starting zoom
-// });
-
 const key = "Sxim0MmX1cOFsGNNYJ49"
 var map = new maplibregl.Map({
     container: 'map',
     style: `https://api.maptiler.com/maps/streets/style.json?key=${key}`, // stylesheet location
-    center: [-74.5, 40], // starting position [lng, lat]
+    center: coordinates, // starting position [lng, lat]
     zoom: 9 // starting zoom
 });
